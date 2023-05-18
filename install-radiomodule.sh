257d658489ad5961210bb856924a222d37e7838b
Joeri Van Dooren <ure@on3ure.be>
2023-05-18 13:38:15 +0200

    auto install scripts

---
+++ install-radiomodule.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+run() {
+  exec=$1
+  printf "\x1b[38;5;104m --> ${exec}\x1b[39m\n"
+  eval ${exec}
+}
+
+say () {
+  say=$1
+  printf "\x1b[38;5;220m${say}\x1b[38;5;255m\n"
+}
+
+say "Upgrading PI"
+run "apt -y update"
+run "apt -y upgrade"
+
+say "Installing Prerequisites"
+run "apt -y install python3-pip git python3-pyaudio python3-scipy"
+
+say "Installing SA818 Control Software"
+run "pip3 install sa818"
+
+say "Installing WM8960 audio intgerface"
+# test audio files https://www2.cs.uic.edu/~i101/SoundFiles/
+run "git clone https://github.com/waveshare/WM8960-Audio-HAT"
+cd WM8960-Audio-HAT/
+run "./install.sh"
+cd ..
+
+# Disable hdmi-audio and enable serial uart
+say "Disabling HDMI audio"
+run "perl -i -pe 's/dtoverlay=vc4-kms-v3d/dtoverlay=vc4-kms-v3d,noaudio/g' /boot/config.txt"
+say "Disabling Serial Console"
+run "perl -i -pe 's/console=serial0.115200//g'  /boot/cmdline.txt"
+say "Enabling UART"
+run "grep -q 'enable_uart=1' /boot/config.txt || echo 'enable_uart=1' >> /boot/config.txt"
+say "Disabling serial getty"
+run 'systemctl disable serial-getty@ttyS0.service'
+
+say "Installing hotspot eeprom configurator"
+run "cp hotspot /usr/sbin"
+run "chmod a+x /usr/sbin/hotspot"
+
+say "Write Initial EEPROM settings to the radio module"
+run "/usr/sbin/hotspot"
+
+say "Installing hotspot_volume"
+run "cp hotspot_volume /usr/sbin/hotspot_volume"
+
+say "Edit & rerun /usr/sbin/hotspot to change frequencies/ctcss etc .."
+say "You only need to run this once as the module has eeprom !"
+run "chmod a+x /usr/sbin/hotspot_volume"
+
+
+say "Please reboot system."
---
+++ install-svxlink.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+
+run() {
+  exec=$1
+  printf "\x1b[38;5;104m --> ${exec}\x1b[39m\n"
+  eval ${exec}
+}
+
+say () {
+  say=$1
+  printf "\x1b[38;5;220m${say}\x1b[38;5;255m\n"
+}
+
+say "Installing SVXLink Prerequisites"
+run "apt install build-essential g++ make cmake libsigc++-2.0-dev php8.0 libgsm1-dev libudev-dev libpopt-dev tcl-dev libgpiod-dev gpiod libgcrypt20-dev libspeex-dev libasound2-dev alsa-utils libjsoncpp-dev libopus-dev rtl-sdr libcurl4-openssl-dev libogg-dev librtlsdr-dev groff doxygen graphviz python3-serial toilet -y"
+
+say "Adding svxlink user and groups"
+run "groupadd svxlink"
+run "useradd -g svxlink -d /etc/svxlink svxlink"
+run "usermod -aG audio,nogroup,svxlink,plugdev svxlink"
+run "usermod -aG gpio svxlink"
+
+say "Installing/Compiling SVXLink"
+run "git clone https://github.com/sm0svx/svxlink.git"
+run "mkdir svxlink/src/build"
+run "cd svxlink/src/build/"
+run "cmake -DUSE_QT=OFF -DCMAKE_INSTALL_PREFIX=/usr -DSYSCONF_INSTALL_DIR=/etc \ -DLOCAL_STATE_DIR=/var -DWITH_SYSTEMD=ON  .."
+run "make"
+run "make doc"
+run "make install"
+cd -
+
+say "Creating events.d symlink"	
+run "cd /usr/share/svxlink/events.d"
+run "ln -s . local"
+cd -
+
+say "Updating LD"
+run "ldconfig"
+
+say "Installing svxlink sounds"
+run "cd /usr/share/svxlink/sounds"
+run "wget https://github.com/sm0svx/svxlink-sounds-en_US-heather/releases/download/19.09/svxlink-sounds-en_US-heather-16k-19.09.tar.bz2"
+run "tar -xvjf svxlink-sounds-en_US-heather-16k-19.09.tar.bz2"
+run "rm *.tar.bz2"
+run "ln -s en_US-heather-16k/ en_US"
+
+say "Install svxlink_rotate"
+run "cp svxlink_rotate /usr/sbin"
+run "chmod a+x /usr/sbin/svxlink_rotate"
+run "ln -s /usr/sbin/svxlink_rotate /etc/cron.daily/svxlink_rotate"
+
+say "Updating svxlink.service"
+run "cp svxlink.service /lib/systemd/system/svxlink.service"
+
+say "Installing default configurations"
+run "cat gpio.conf > /etc/svxlink/gpio.conf"
+run "cat svxlink.conf > /etc/svxlink/svxlink.conf"
+run "cat svxlink_gpio_up > /usr/sbin/svxlink_gpio_up"
+
+say "Installing systemd services"
+run "systemctl enable svxlink_gpio_setup"
+run "systemctl enable svxlink"
+run "systemctl start svxlink_gpio_setup.service"
+run "systemctl start svxlink.service"
+
+say "Svxlink config is in here: /etc/svxlink/svxlink.conf"
+
+
+
+
+
+
