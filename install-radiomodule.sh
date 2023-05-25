63282c573c23a6aea1004cdf971b327574bc3cbc
Joeri Van Dooren <ure@on3ure.be>
2023-05-25 19:41:56 +0200

    fix locale

--- install-radiomodule.sh
+++ install-radiomodule.sh
@@ -26,6 +26,8 @@ run "echo 'LANG=en_US.UTF-8' > /etc/default/locale"
 run "echo 'LC_CTYPE=en_US.UTF-8' >> /etc/default/locale"
 run "echo 'LC_MESSAGES=en_US.UTF-8' >> /etc/default/locale"
 run "echo 'LC_ALL=en_US.UTF-8' >> /etc/default/locale"
+run "echo 'en_US.UTF-8 UTF-8' > /etc/locale.gen"
+
 run "locale-gen"
 
 say "Installing WM8960 audio intgerface"
--- install-svxlink.sh
+++ install-svxlink.sh
@@ -16,6 +16,10 @@ MYPATH=${PWD}
 say "Installing SVXLink Prerequisites"
 run "apt install build-essential g++ make cmake libsigc++-2.0-dev php8.0 libgsm1-dev libudev-dev libpopt-dev tcl-dev libgpiod-dev gpiod libgcrypt20-dev libspeex-dev libasound2-dev alsa-utils libjsoncpp-dev libopus-dev rtl-sdr libcurl4-openssl-dev libogg-dev librtlsdr-dev groff doxygen graphviz python3-serial toilet -y"
 
+say "Disabling apache"
+run "systemctl disable apache2"
+run "systemctl disable apache-htcacheclean"
+
 say "Adding svxlink user and groups"
 run "groupadd svxlink"
 run "useradd -g svxlink -d /etc/svxlink svxlink"
