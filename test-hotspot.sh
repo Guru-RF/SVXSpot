1948b5a6308bab02f72f67cc7a1ae65562b8cb63
Joeri Van Dooren <ure@on3ure.be>
2023-12-14 15:47:26 +0100

    fix

--- test-hotspot.sh
+++ test-hotspot.sh
@@ -1,15 +1,37 @@
 #!/bin/bash
 
-systemctl stop svxlink
-curl -L -o /tmp/ImperialMarch60.wav https://github.com/Guru-RF/SVXSpot/raw/master/ImperialMarch60.wav
-/usr/sbin/hotspot_volume > /dev/null 2>&1
-/usr/sbin/hotspot
-raspi-gpio set 16 dl
-aplay /tmp/ImperialMarch60.wav
-raspi-gpio set 16 dh
-echo "Send audio with your portable ... recording for 20seconds"
-arecord -V stereo -r 44100 -f S16_LE -c 2 /tmp/test.wav -d 20
-echo "Playing recording"
-raspi-gpio set 16 dl
-aplay /tmp/test.wav
-raspi-gpio set 16 dh
+run() {
+  exec=$1
+  printf "\x1b[38;5;104m --> ${exec}\x1b[39m\n"
+  eval ${exec}
+}
+
+say () {
+  say=$1
+  printf "\x1b[38;5;220m${say}\x1b[38;5;255m\n"
+}
+
+say "Stopping svxlink"
+run "systemctl stop svxlink"
+say "Downloading Imperial March 60 seconds test audio"
+run "curl -L -o /tmp/ImperialMarch60.wav https://github.com/Guru-RF/SVXSpot/raw/master/ImperialMarch60.wav > /dev/null 2>&1"
+say "Setting hotspot volume"
+run "/usr/sbin/hotspot_volume > /dev/null 2>&1"
+say "Configuring RF module"
+run "/usr/sbin/hotspot"
+say "PTT on"
+run "raspi-gpio set 16 dl"
+say "Play Imperail March for 60 seconds"
+run "aplay /tmp/ImperialMarch60.wav"
+say "PTT off"
+run "raspi-gpio set 16 dh"
+say "Send audio with your portable ... recording for 20seconds"
+run "arecord -V stereo -r 44100 -f S16_LE -c 2 /tmp/test.wav -d 20"
+say "PTT on"
+run "raspi-gpio set 16 dl"
+say "Playing recording"
+run "aplay /tmp/test.wav"
+say "PTT off"
+run "raspi-gpio set 16 dh"
+say "Turning svxlink back on"
+run "systemctl start svxlink"
