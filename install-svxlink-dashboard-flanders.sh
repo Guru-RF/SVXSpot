ef263c1aac3ce2d216a2f564c1115d51bf9117c5
Joeri Van Dooren <ure@rf.guru>
2023-12-24 20:23:53 +0100

    add dashboard

---
+++ install-svxlink-dashboard-flanders.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+run() {
+  exec=$1
+  printf "\x1b[38;5;104m --> ${exec}\x1b[39m\n"
+  eval ${exec}
+}
+
+say () {
+  say=$1
+  printf "\x1b[38;5;220m${say}\x1b[38;5;255m\n"
+}
+
+MYPATH=${PWD}
+
+say "Install php.ini"
+run "cp php.ini /etc/php/8.2/apache2/php.ini"
+
+say "Enable Apache"
+run "systemctl enable apache2"
+run "systemctl enable apache-htcacheclean"
+
+say "Remove default html dir"
+run "rm -fr /var/www/html"
+
+say "Clone dashboard from repo"
+run "cd /var/www; git clone https://github.com/Guru-RF/SVXLink-Dash-V2-FLANDERS.git html"
+
+say "Start Apache"
+run "systemct start apache2"
+
+HOSTNAME=$(hostname)
+say "You should be able to access the dashboard via: http://${HOSTNAME}.local"
+
+
+
+
+
+
+
