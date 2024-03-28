148e2fe40930fe5baee25123ab5de48bbaf858a2
ure <ure@moreorless.be>
2024-03-28 13:13:21 +0100

    remove old kernels

---
+++ remove-old-kernels.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# Run this script without any param for a dry run
+# Run the script with root and with exec param for removing old kernels after checking
+# the list printed in the dry run
+# From: 
+
+uname -a
+IN_USE=$(uname -a | awk '{ print $3 }')
+echo "Your in use kernel is $IN_USE"
+
+OLD_KERNELS=$(
+    dpkg --list |
+        grep -v "$IN_USE" |
+        grep -Ei 'linux-image|linux-headers|linux-modules' |
+        awk '{ print $2 }'
+)
+echo "Old Kernels to be removed:"
+echo "$OLD_KERNELS"
+
+if [ "$1" == "exec" ]; then
+    for PACKAGE in $OLD_KERNELS; do
+        yes | apt purge "$PACKAGE"
+    done
+fi
