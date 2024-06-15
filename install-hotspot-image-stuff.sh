431ea7483f3e639f4805e4d6b1dc4fa1c5ec166b
Joeri Van Dooren <ure@moreorless.be>
2024-06-15 12:46:14 +0200

    fix issues and requested enhancements

---
+++ install-hotspot-image-stuff.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+run() {
+  exec=$1
+  printf "\x1b[38;5;104m --> ${exec}\x1b[39m\n"
+  eval ${exec}
+}
+
+say () {
+  say=$1
+  printf "\x1b[38;5;220m${say}\x1b[38;5;255m\n"
+}
+
+MYPATH=${PWD}
+
+say "Install /usr/sbin/mylocalip"
+run "cp mylocalip /usr/sbin/mylocalip"
+
+say "Install /usr/sbin/svxlink-message"
+run "cp svxlink-message /usr/sbin/svxlink-message"
+
+say "Install /usr/sbin/svxlink-internet-checker"
+run "cp svxlink-internet-checker /usr/sbin/svxlink-internet-checker"
+
+say "Install /etc/cron.d/checkinternet"
+run "cp checkinternet /etc/cron.d/checkinternet"
+
+say "Install /usr/share/svxlink/events.d/local/Logic.tcl"
+run "cp Logic.tcl /usr/share/svxlink/events.d/local/Logic.tcl"
+
+say "Install shellinabox"
+say "apt -y install shellinabox"
+
+say "Enable/Start shellinabox"
+run "systemctl enable shellinabox"
+run "systemctl start shellinabox"
+
+say "White on Black shellinabos by default"
+run "cd /etc/shellinabox/options-enabled && mv 00+Black\ on\ White.css 00_Black\ on\ White.css"
+
+say "Install https://www.raspberryconnect.com/projects/65-raspberrypi-hotspot-accesspoints/203-automated-switching-accesspoint-wifi-network manually"
+
+say "Regenerate ssh keys via ./generate-ssh-keys.sh"
+
+
+
+
+
+
+
