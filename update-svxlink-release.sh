a32a2a1a8f0476b99e46f830fd1d7f73e16ffc1c
Joeri Van Dooren <ure@moreorless.be>
2024-07-31 14:56:04 +0200

    svxlink update main/release

---
+++ update-svxlink-release.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+run() {
+  exec=$1
+  printf "\x1b[38;5;104m --> ${exec}\x1b[39m\n"
+  eval ${exec}
+}
+
+say () {
+  say=$1
+  printf "\x1b[38;5;220m${say}\x1b[38;5;255m\n"
+}
+
+if [ "$EUID" -ne 0 ]
+  then say "Please run as root"
+  exit
+fi
+
+MYPATH=${PWD}
+
+cd /tmp
+
+say "Run apt autoremove"
+run "apt -y autoremove"
+
+say "Installing SVXLink Prerequisites"
+run "apt install libssl-dev ladspa-sdk moreutils build-essential g++ make cmake libsigc++-2.0-dev php libgsm1-dev libudev-dev libpopt-dev tcl-dev libgpiod-dev gpiod libgcrypt20-dev libspeex-dev libasound2-dev alsa-utils libjsoncpp-dev libopus-dev rtl-sdr libcurl4-openssl-dev libogg-dev librtlsdr-dev groff doxygen graphviz python3-serial toilet sox bc avahi-daemon avahi-utils -y"
+
+say "Installing/Compiling SVXLink"
+run "git clone --branch maint https://github.com/sm0svx/svxlink.git"
+run "mkdir svxlink/src/build"
+run "cd svxlink/src/build/"
+run "cmake -DUSE_QT=OFF -DCMAKE_INSTALL_PREFIX=/usr -DSYSCONF_INSTALL_DIR=/etc -DLOCAL_STATE_DIR=/var -DWITH_SYSTEMD=ON  .."
+run "make -j1"
+run "make doc"
+run "make install"
+
+say "Updating Logic.tcl"
+run "wget https://raw.githubusercontent.com/Guru-RF/SVXSpot/master/Logic.tcl"
+run "cp Logic.tcl /usr/share/svxlink/events.d/local/Logic.tcl"
+
+say "Updating svxlink.service"
+run "wget https://raw.githubusercontent.com/Guru-RF/SVXSpot/master/svxlink.service"
+run "cp svxlink.service /lib/systemd/system/svxlink.service"
+
+say "Updating remotetrx.service"
+run "wget https://raw.githubusercontent.com/Guru-RF/SVXSpot/master/remotetrx.service"
+run "cp remotetrx.service /lib/systemd/system/remotetrx.service"
+
+say "Cleanup"
+run "cd /tmp && rm -fr svxlink"
+
+say "Restart svxlink"
+run "systemctl daemon-reload"
+run "systemctl restart svxlink"
+
+cd ${MYPATH}
