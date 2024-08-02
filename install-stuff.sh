f89c07aa885b0d429826ddbf35f9fe3dbee6a798
Joeri van Dooren <joeri@weepee.org>
2024-08-02 08:21:14 +0200

    Create install-stuff.sh

---
+++ install-stuff.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+run() {
+  exec=$1
+  printf "\x1b[38;5;104m --> ${exec}\x1b[39m\n"
+  eval ${exec}
+}
+
+say () {
+  say=$1
+  printf "\x1b[38;5;220m${say}\x1b[38;5;255m\n"
+}
+
+MYPATH=${PWD}
+
+cd /root
+go install github.com/maxmcd/webtty@latest
+cp go/bin/webtty /usr/sbin
+rm -fr go
